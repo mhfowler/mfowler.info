57c4ae570158d27ca3958bf9b6b71c1802bb9ce3
Max Fowler <maxhfowler@gmail.com>
2016-02-09 10:17:23 -0500

    stuff

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+
+from flask import Flask, send_from_directory
+from flask import render_template
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+TEMPLATE_DIR = os.path.join(PROJECT_PATH, 'templates')
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=PROJECT_PATH)
+
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+
 
 @app.route('/')
 def mfowler():
-    return 'Hello World!'
+     return render_template("mfowler_info.html")
 
 if __name__ == '__main__':
     app.run()
--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,30 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var exec = require('child_process').exec;
+var sleep = require('sleep');
+
+
+var flask_proc;
 
 // flask server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
-        server: "./"
+        notify: false,
+        proxy: "127.0.0.1:5000"
     });
 
-    var proc = exec('python app.py');
+    flask_proc = exec('python app.py');
 
     gulp.watch("scss/*.scss", ['sass']);
-    gulp.watch(["html/*.html","css/*.css"]).on('change', browserSync.reload);
+    gulp.watch(["templates/*.html","static/css/*.css"]).on('change', browserSync.reload);
 });
 
 // Compile sass into CSS
 gulp.task('sass', function() {
     return gulp.src("scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("static/css"))
         ;
 });
 
